5e4c9815248080cb86c6cd495c2999b72d6b11d8
Snir Sharristh <snirs@wix.com>
2024-11-25 11:56:13 +0200

    first working state

--- src/app/components/game-controller/gameSlice.ts
+++ src/app/components/game-controller/gameSlice.ts
@@ -1,4 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { Simulate } from 'react-dom/test-utils';
+import toggle = Simulate.toggle;
 
 const GRID_SIZE = 30;
 
@@ -16,6 +18,63 @@ const initialState: GameState = {
 	isRunning: false,
 };
 
+type Coord = [number, number]
+type ActiveNeighbors = Coord[]
+
+const NEIGHBOR_COORDINATES = [
+	[-1, 0], [1, 0], [0, -1], [0, 1],
+	[-1, -1], [-1, 1], [1, -1], [1, 1]
+]
+
+const getNeighbors =  (gridData: GridData, row: number, col: number, active: boolean) => {
+	return NEIGHBOR_COORDINATES.reduce<ActiveNeighbors>((acc, [rowOffset, colOffset]) => {
+		if (row + rowOffset < 0 || row + rowOffset >= GRID_SIZE) {
+			return acc;
+		}
+		if (col + colOffset < 0 || col + colOffset >= GRID_SIZE) {
+			return acc;
+		}
+		const isActive = gridData[row + rowOffset][col + colOffset];
+		if (isActive === active) acc.push([row + rowOffset, col + colOffset]);
+		return acc;
+	}, [])
+}
+
+const getActiveNeightbors = (gridData: GridData, row: number, col: number) => {
+	return getNeighbors(gridData, row, col, true);
+}
+
+const getNonActiveNeighbors = (gridData: GridData, row: number, col: number) => {
+	return getNeighbors(gridData, row, col, false);
+}
+
+const toggledCells: {[key: string]: boolean} = {}
+
+function updateToggledCells(gridData: GridData, row: number, col: number) {
+	if (gridData[row][col]) {
+		toggledCells[`${row}-${col}`] = gridData[row][col];
+	} else {
+		delete toggledCells[`${row}-${col}`];
+	}
+}
+
+function handleActiveNeighbors(gridData: GridData, row: number, col: number, toUpdate: { coords: Coord; active: boolean }[]) {
+	const activeNeighbors = getActiveNeightbors(gridData, row, col);
+	if (activeNeighbors.length < 2 || activeNeighbors.length > 3) {
+		toUpdate.push({ coords: [row, col], active: false });
+	}
+}
+
+function handleNonActiveNeighbors(gridData: GridData, row: number, col: number, toUpdate: { coords: Coord; active: boolean }[]) {
+	const inActiveNeighbors = getNonActiveNeighbors(gridData, row, col);
+	for (const [nrow, ncol] of inActiveNeighbors) {
+		const activeNeightborOfNonActive = getActiveNeightbors(gridData, nrow, ncol);
+		if (activeNeightborOfNonActive.length === 3) {
+			toUpdate.push({ coords: [nrow, ncol], active: true });
+		}
+	}
+}
+
 const gameSlice = createSlice({
 	name: 'game',
 	initialState,
@@ -23,6 +82,7 @@ const gameSlice = createSlice({
 		toggleCell: (state, action: PayloadAction<{ row: number; col: number }>) => {
 			const { row, col } = action.payload;
 			state.gridData[row][col] = !state.gridData[row][col];
+			updateToggledCells(state.gridData, row, col);
 		},
 		startGame: (state) => {
 			state.isRunning = true;
@@ -36,11 +96,21 @@ const gameSlice = createSlice({
 				.map(() => Array(GRID_SIZE).fill(false));
 			state.isRunning = false;
 		},
-		tick: (state, payload) => {
-			console.warn('tick is not implemented', {
-				state,
-				payload,
-			});
+		tick: (state) => {
+			const toUpdate: {coords: Coord, active: boolean}[] = []
+			for (const cell in toggledCells){
+				const [row, col] = cell.split('-').map(Number);
+				handleActiveNeighbors(state.gridData, row, col, toUpdate);
+				handleNonActiveNeighbors(state.gridData, row, col, toUpdate);
+			}
+			for (const {coords, active} of toUpdate){
+				state.gridData[coords[0]][coords[1]] = active;
+				if(active){
+					toggledCells[`${coords[0]}-${coords[1]}`] = active;
+				} else {
+					delete toggledCells[`${coords[0]}-${coords[1]}`];
+				}
+			}
 		},
 	},
 });
